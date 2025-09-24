6f949d3370a6d9986c41d698d3e2ed3386ac05e4
thanuj301105 <malkajgirithanuj32@gmail.com>
2025-09-24 02:36:08 -0700

    Update Sample_Program.dart

--- Sample_Program.dart
+++ Sample_Program.dart
@@ -1,8 +1,9 @@
 void main() {
-  String name = "Pranavika";
+  String name = "Thanuj";
   print(name);
   List<String> Skills = ['c', 'c++', 'Dart'];
   print(Skills[0]);
   int sum = 3 + 5;
   print("Sum:$sum");
 }
+
